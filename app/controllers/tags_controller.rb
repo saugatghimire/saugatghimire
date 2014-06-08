dce9cfe4eaabb5e2bf498b60634428b905a57206
Saugat Ghimire <sagydada@gmail.com>
2014-06-08 00:34:56 -0400

    Added authorization to tags

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,4 +1,6 @@
 class TagsController < ApplicationController
+  before_action :signed_in_user, except: [:index, :show]
+  
   def index
     @tags = Tag.all
   end
@@ -15,4 +17,13 @@ class TagsController < ApplicationController
 
     redirect_to tags_path
   end
+
+  private
+
+    def signed_in_user
+      unless signed_in?
+        flash[:warning] = "Please sign in!"
+        redirect_to signin_url
+      end
+    end
 end
